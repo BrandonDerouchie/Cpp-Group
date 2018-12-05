7b6b149eb949b9f5958df0ac7f3b81be912bd939
BrandonDerouchie <43012534+BrandonDerouchie@users.noreply.github.com>
2018-12-05 13:03:31 -0500

    Create group.cpp

---
+++ group.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+int car (bool x)
+{cin >> x;
+if (x = yes)
+{
+    ++yes;
+    return x;
+}}
+int main ()
+int yes[] = {0,0,0,0,0};
+int no[] = {0,0,0,0,0};
+cout << "do i know the true value of the car?";
+char c-value;
+cin >> c-value;
+if (c-value = yes)
+{
+    ++yes;
+}
+cout << "Less than 5 owners?";
+cin >> c-value;
+if (c-value = yes)
+{
+    ++yes;
+}
+cout << "less than 200,000Km?";
+cin >> c-value;
+if (c-value = yes)
+{
+    ++yes;
+}
+cout << "is the car free of any accidents?";
+cin >> c-value;
+if (c-value = yes)
+{
+    ++yes;
+}
+cout << "Is the car road ready?";
+cin >> c-value;
+if (c-value = yes)
+{
+    ++yes;
+}
+cout << "Wait while we calculate your results";
+if(yes >=3){
+cout << "Buy the car!";
+}
+
+
+
+
+
+return 0;
