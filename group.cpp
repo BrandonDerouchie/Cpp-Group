80431a60f23400fda0bc120966ce08360be9eaf7
cmajor99 <corymajor72@gmail.com>
2018-12-05 15:58:43 -0500

    edit 1

--- group.cpp
+++ group.cpp
@@ -2,35 +2,38 @@
 using namespace std;
 // pass by reference for array
  int car (int x) {
-for(int i=0; i<5; i++)
-cin >> x;
+    //  int x;
+//  for(int i=0; i<5; i++)
+// cin >> x;
 return x; }
 int main ()
 { 
      while(1 == 1){
 // for each
-char x;
+int x;
+
 int yes[5] = {0,0,0,0,0};
 int no[5] = {0,0,0,0,0};
 cout << "do i know the true value of the car?";
-char cvalue;
-if(x == yes) {
+// char cvalue;
+cin >> x;
+if(x == 1) {
    car(x) = yes[1]; 
 }
 cout << "Less than 5 owners?";
-if(x == yes) {
+if(x == 1) {
    car(x) = yes[2]; 
 }
 cout << "less than 200,000Km?";
-if(x == yes) {
+if(x == 1) {
    car(x) = yes[3]; 
 }
 cout << "is the car free of any accidents?";
-if(x == yes) {
+if(x == 1) {
    car(x) = yes[4]; 
 }
 cout << "Is the car road ready?";
-if(x == yes) {
+if(x == 1) {
    car(x) = yes[5]; 
 }
 cout << "Wait while we calculate your results";
