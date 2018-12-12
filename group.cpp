014b6bcaa30a698edab0e0716f92850454c447e0
cmajor99 <corymajor72@gmail.com>
2018-12-12 12:10:57 -0500

    fixes

--- group.cpp
+++ group.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <dos.h>
 using namespace std;
  int car (int x) {
  cin >> x;
@@ -13,50 +14,56 @@ int no[5] = {0,0,0,0,0};
 cout << "welcome to how to buy a car.\n select your answers with 0, 1, 2.\n";
 cout << " 0 = no\n 1 = yes\n 2 = exit\n";
 cout << "do i know the true value of the car?\n";
-if(car(x) == 2){
+int result = car(x);
+if(result == 2){
     return 0;
 }
-if(car(x) == 1){
-   yes[0] = x;
+else if(result == 1){
+   yes[0] = 1;
 }
-else if(car(x) == 0){
-    no[0] = x;
+else if(result == 0){
+    no[0] = 1;
 }
 cout << "Less than 5 owners?\n";
-if(car(x) == 1) {
-   yes[1] = x; 
+result = car(x);
+if(result == 1) {
+   yes[1] = 1; 
 }
-else if(car(x) == 0){
-    no[1] = x;
+else if(result == 0){
+    no[1] = 1;
 }
 cout << "less than 200,000Km?\n";
-if(car(x) == 1) {
-    yes[2] = x; 
+result = car(x);
+if(result == 1) {
+    yes[2] = 1; 
 }
-else if(car(x) == 0){
-    no[2] = x;
+else if(result == 0){
+    no[2] = 1;
 }
 cout << "is the car free of any accidents?\n";
-if(car(x) == 1) {
-   yes[3] = x; 
+result = car(x);
+if(result == 1) {
+   yes[3] = 1; 
 }
-else if(car(x) == 0){
-    no[3] = x;
+else if(result == 0){
+    no[3] = 1;
 }
 cout << "Is the car road ready?\n";
-if(car(x) == 1) {
-   yes[4] = x; 
+result = car(x);
+if(result == 1) {
+   yes[4] = 1; 
 }
-else if(car(x) == 0){
-    no[4] = x;
+else if(result == 0){
+    no[4] = 1;
 }
 cout << "Wait while we calculate your results\n";
+cout << "..\n..\n..\n..\n..\n..\n..\n..\n..\n..\n..\n..\n..\n";
 y = yes[0] + yes[1] + yes[2] + yes[3] + yes[4];
 n = no[0] + no[1] + no[2] + no[3] + no[4];
  if(y >=3){
     cout << "Buy the car!\n";
  }
- else if(n >=3){
+ if(n >=3){
     cout << "Don't buy the car!\n";
  }
 
